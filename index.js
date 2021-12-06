ab677fd0392be25c0875539cbbd8cf33be97d3ac
Robin <roblan95@icloud.com>
2021-12-06 03:06:27 +0100

    add .env

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.DEV_URL || "https://admiring-jang-94e13e.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
